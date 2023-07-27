8a454d58b2871c61e3495725655d63fcbbe5d0f6
Muhd. Zulhilmi Abdullah <muhdzulhilmiabdullah@gmail.com>
2023-07-27 10:22:11 +0800

    update voting page

--- resources/views/votingPage.blade.php
+++ resources/views/votingPage.blade.php
@@ -1,19 +1,34 @@
-@extends('layouts.app')
+@extends('layouts.publicapp')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Voting Page</div>
+                <div class="card-header text-center">Voting Page</div>
 
                 <div class="card-body">
-                    @foreach ($votingPositions as $item)
-                            <h3>Position: {{ $item->positionNm }}</h3><br>
-                            <!-- You can add any other information related to the position here -->
-                            <a type="button" class="btn btn-primary" href="{{ route('finalVotingPage', $item->id) }}">Click to Vote</a>
-                        <br>
-                    @endforeach
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th class="text-center">Position</th>
+                                <th class="text-center">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($votingPositions as $item)
+                                <tr>
+                                    <td class="text-center">
+                                        <h3>{{ $item->positionNm }}</h3>
+                                        <!-- You can add any other information related to the position here -->
+                                    </td>
+                                    <td class="text-center">
+                                        <a type="button" class="btn btn-primary d-flex justify-content-center" href="{{ route('finalVotingPage', $item->id) }}">Click to Vote</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
                 <br>
             </div>
